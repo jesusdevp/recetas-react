ea1abbe2825306e8e2e7d68d00fbc96fc0857ea1
Jesus Ramirez <jesusdevelp@gmail.com>
2020-06-24 11:55:22 -0500

    Primeros Ajustes

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const Formulario = () => {
+  return (
+    <form className="col-12">
+      <fieldset className="text-center">
+        <legend>Busca bebidas por Categoría o Ingrediente</legend>
+      </fieldset>
+      <div className="row mt-4">
+        <div className="col-md-4">
+          <input
+            type="text"
+            className="form-control"
+            name="nombre"
+            placeholder="Buscar por Ingrediente"
+          />
+        </div>
+        <div className="col-md-4">
+          <select className="form-control" name="categoria">
+            <option value="">-- Selecciona Categoria --</option>
+          </select>
+        </div>
+        <div className="col-md-4">
+          <input
+            type="submit"
+            className="btn btn-block btn-primary"
+            value="Buscar Bebidas"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default Formulario;
